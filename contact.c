102c11505f451a0d6d6b664b504868f76c25481c
Kenza <kenza.braham-chaouch@efrei.net>
2023-12-09 15:25:25 +0100

    [creation contact files]

---
+++ contact.c
@@ -0,0 +1,10 @@
+//
+// Created by kenza on 09/12/2023.
+//
+
+#include "contact.h"
+#include <ctype.h>
+#include "stdlib.h"
+#include "stdio.h"
+#include "string.h"
+
---
+++ contact.h
@@ -0,0 +1,9 @@
+//
+// Created by kenza on 09/12/2023.
+//
+
+#ifndef PROJETC_S3_FINAL_CONTACT_H
+#define PROJETC_S3_FINAL_CONTACT_H
+
+
+#endif //PROJETC_S3_FINAL_CONTACT_H
